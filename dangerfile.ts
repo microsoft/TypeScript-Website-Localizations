3f930440cb8eaeda1748c96e62aa84d8d8e2ab8c
Orta <git@orta.io>
2021-01-25 21:13:07 +0000

    Adds inline translate

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,15 +1,21 @@
-// const markdownTranslate = require('markdown-translator')
-// markdownTranslate({
-//   src: pathToSrcFile,
-//   from: languageToTranslateFrom,
-//   to: languageToTranslateTo,
-//   subscriptionKey: yourSubscriptionKey,
-//   region: theRegionOfYourAzureInstance
-// }).then(res => {
-
-// })
-
+const markdownTranslate = require('@orta/markdown-translator')
 import {danger, message} from "danger"
 
-const allFiles = [...danger.git.created_files, ...danger.git.created_files]
-message(allFiles.join(", "))
+const go = async () => {
+  const allMDFiles = [...danger.git.modified_files, ...danger.git.created_files].filter(f => f.endsWith(".md")).filter(f => f.split("/").length > 2)
+  console.log(allMDFiles)
+  for (const file of allMDFiles) {
+    const fileContents = await danger.github.utils.fileContents(file)
+    const translation = await markdownTranslate({
+      text: fileContents,
+      from: file.split("/")[2],
+      to: "en",
+      subscriptionKey: process.env.AZURE_TRANSLATE_KEY,
+      region: "eastus"
+    })
+    
+    message(`Translation of ${danger.github.utils.fileLinks([file])}\n\n${translation}`)
+  };
+}
+
+go()
