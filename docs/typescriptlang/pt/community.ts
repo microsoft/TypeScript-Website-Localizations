213303e45c00f005c0b5ecf27a1a53ee3016d0e1
Yuri Withowsky <yuri.jivago14@gmail.com>
2021-11-18 16:04:35 -0300

    fix: standardizing the TypeScript name in Portuguese documentation

--- docs/typescriptlang/pt/community.ts
+++ docs/typescriptlang/pt/community.ts
@@ -7,7 +7,7 @@ export const comCopy = {
   com_connect_online_description:
     "Conte-nos o que está funcionando bem, o que quer ver adicionado ou aprimorado e descubra novas atualizações.",
   com_online_stack_overflow_desc:
-    "Conecte-se com colegas e faça perguntas sobre Typescript usando a tag 'typescript'",
+    "Conecte-se com colegas e faça perguntas sobre TypeScript usando a tag 'typescript'",
   com_online_stack_overflow_tag: "",
   com_online_discord_header: "Chat",
   com_online_discord_desc:
@@ -17,8 +17,8 @@ export const comCopy = {
   com_online_github_href: "Conte-nos no Github",
   com_online_twitter_desc: "Mantenha-se atualizado. Nos siga no Twitter",
   com_online_blog_desc:
-    "Aprenda sobre os desenvolvimentos mais recentes do Typescript no nosso", //... blog
-  com_online_typed_desc: "Definições de tipos de Typescript.",
+    "Aprenda sobre os desenvolvimentos mais recentes do TypeScript no nosso", //... blog
+  com_online_typed_desc: "Definições de tipos de TypeScript.",
   com_online_typed_href: "Navegue pelas milhares de",
   com_online_typed_available_for: "bibliotecas e frameworks disponíveis.",
   com_person: "Conecte-se pessoalmente",
